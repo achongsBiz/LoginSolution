c43dfb8137c6afcad9962df0029d2cc39012abbb
achongsBiz <38401255+achongsBiz@users.noreply.github.com>
2018-05-16 21:28:32 -0400

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,31 @@
+<html>
+<title>
+Registration Page
+</title>
+<body>
+
+<h1>Please fill out the following form to register</h1>
+<form action="registrationProcess.php" method="post">
+
+<table>
+<tr><td>username:</td><td><input type="text" name="username"></td></tr>
+<tr><td>password:</td><td><input type="password" name="credential"></td></tr><br>
+<tr><td>first name:</td><td> <input type="text" name="firstname"><br>
+<tr><td>last name:</td><td> <input type="text" name="lastname"><br>
+
+<tr><td>Street Address first line:</td><td> <input type="text" name="street_line1"></td></tr>
+<tr><td>Street Address second line:</td><td> <input type="text" name="street_line2"></td></tr>
+<tr><td>City:</td><td> <input type="text" name="city"></td></tr>
+<tr><td>Region:</td><td> <input type="text" name="region"></td></tr>
+<tr><td>Postal Code:</td><td> <input type="text" name="postal"></td></tr>
+<tr><td>Country:</td><td> <input type="text" name="country"></td></tr>
+
+<tr><td>Email:</td><td>  <input type="text" name="email"></td></tr>
+<tr><td>Phone:</td><td>  <input type="text" name="phone"></td></tr>
+</table>
+
+<input type="submit">
+</form>
+
+</body>
+</html>
